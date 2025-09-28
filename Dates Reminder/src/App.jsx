4bdfb57464e55387f55695619b2148c81f436054
unknown <amrm96754@gmail.com>
2025-09-28 16:05:08 +0300

    setting structure of Date Reminders Project

---
+++ Dates Reminder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+
+const App = () => {
+  return (
+    <div>
+      <Button className='btn btn-primary'>Test</Button>
+    </div>
+  )
+}
+
+export default App
